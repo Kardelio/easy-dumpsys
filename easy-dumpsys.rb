5ef20797f4b68f25a47b31e3dcba6d60e36d93ae
kardelio <kardeliogithub@outlook.com>
2021-03-25 18:41:00 +0100

    Added brew formula

---
+++ easy-dumpsys.rb
@@ -0,0 +1,11 @@
+class EasyDumpsys < Formula
+  desc "Script that outputs this adb command: `adb shell dumpsys activity <package>` in a far smaller, easier & user-friendlier way..."
+  homepage ""
+  url "https://github.com/Kardelio/easy-dumpsys/archive/refs/tags/v2.0.0.tar.gz"
+  sha256 "c722bb64dcd133f55c4b7b427dbaee29d8d94eb7681777264985f7adc5323277"
+  license "GPL-3.0"
+
+  def install
+    bin.install "easy-dumpsys"
+  end
+end
