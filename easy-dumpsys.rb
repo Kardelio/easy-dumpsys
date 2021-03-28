a99d87670da8c85616a686bdd776f01e6b013810
kardelio <kardeliogithub@outlook.com>
2021-03-28 18:12:02 +0200

    Updated the formula

--- easy-dumpsys.rb
+++ easy-dumpsys.rb
@@ -1,8 +1,8 @@
 class EasyDumpsys < Formula
   desc "Script that outputs this adb command: `adb shell dumpsys activity <package>` in a far smaller, easier & user-friendlier way..."
   homepage ""
-  url "https://github.com/Kardelio/easy-dumpsys/archive/refs/tags/v2.0.0.tar.gz"
-  sha256 "c722bb64dcd133f55c4b7b427dbaee29d8d94eb7681777264985f7adc5323277"
+  url "https://github.com/Kardelio/easy-dumpsys/archive/refs/tags/v2.0.1.tar.gz"
+  sha256 "1bed43730780b2e9d528cc07cb68a9797a44dd47d3b55e9fa03293fe4f0f86ba"
   license "GPL-3.0"
 
   depends_on "fzf"
