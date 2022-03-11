d7bff2f20e20cbc8653b76bb4f010ebf1d149656
kardelio <kardeliogithub@outlook.com>
2022-03-11 13:28:09 +0100

    Brew Package Update

--- easy-dumpsys.rb
+++ easy-dumpsys.rb
@@ -1,8 +1,8 @@
 class EasyDumpsys < Formula
   desc "Script that outputs this adb command: `adb shell dumpsys activity <package>` in a far smaller, easier & user-friendlier way..."
   homepage ""
-  url "https://github.com/Kardelio/easy-dumpsys/archive/refs/tags/v2.1.0.tar.gz"
-  sha256 "0702e789e1f4cf73a77a0210c655e11201a4d27f59ded7aee1f41770929856b4"
+  url "https://github.com/Kardelio/easy-dumpsys/archive/refs/tags/v2.1.1.tar.gz"
+  sha256 "5942b26ac79c2a242882c8f6bca12c239a3ec98a71343bda437e481e601144ba"
   license "GPL-3.0"
 
   depends_on "fzf"
